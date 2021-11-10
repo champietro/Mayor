37e5ade5d5dac58341d899d2925b3cfb2b38746c
Juan Pedro Alcuaz <champietro@users.noreply.github.com>
2021-11-10 19:56:27 -0300

    dsf

--- src/index.ts
+++ src/index.ts
@@ -21,12 +21,12 @@ let mayorValor : number = 40;
 
 let aleatorio = function (menorValor, mayorValor: number) {
 let menores21, MayorIgual : number = 0;
-let edades: string[] = new Array(5);//cambiar el 5 por el 270
+let edades: number[] = new Array(5);//cambiar el 5 por el 270
  
   for (let i = 0; i < 5; i++){///idem
-    edades[i] = prompt("Edad:");
+    //edades[i] = Number(prompt("Edad:"));
   }
- for (let i = 0; i < 5; i++){///idem
+ for (i = 0; i < 5; i++){///idem
  if (edades[i]<21){
    menores21 = menores21 + 1;
  }else{
